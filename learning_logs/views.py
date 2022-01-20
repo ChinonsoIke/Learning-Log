4983ff0cbef8ac374d4b65d263dcd724b313781b
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-01-20 11:19:31 +0100

    Added custom error pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 import imp
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -21,7 +21,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """render specific topic and all it's entries"""
-    topic= Topic.objects.get(id=topic_id)
+    topic= get_object_or_404(Topic, id=topic_id)
     if topic.owner != request.user:
         raise Http404
     entries= topic.entry_set.order_by('-date_added')
